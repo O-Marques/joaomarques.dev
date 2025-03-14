1802d4047a722ccdda9746dd58b38d13f7cc3d7e
O_Marques <joaomarques.wn@gmail.com>
2025-03-14 13:22:18 +0000

    add home page

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import './UnderConstruction.css'; // You can create this CSS file for styling
+
+const UnderConstruction = () => {
+  return (
+    <div className="under-construction">
+      <h1>🚧 Under Construction</h1>
+      <p>We are working hard to bring you an awesome site. Stay tuned!</p>
+    </div>
+  );
+}
+
+export default UnderConstruction;
